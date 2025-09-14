cf634c445ce5f145e6a1afcd38789c525b10e031
Prince Vuha <princevuha@gmail.com>
2025-09-14 18:26:38 +0000

    Updated task_4/js/main.ts

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -6,4 +6,34 @@ namespace Subjects {
       this.teacher = teacher;
     }
   }
+
+  // Create and export constants for each subject
+  export const cpp = new Cpp();
+  export const java = new Java();
+  export const react = new React();
+
+  // Create and export teacher object with C++ experience
+  export const cTeacher: Teacher = {
+    firstName: "John",
+    lastName: "Doe",
+    experienceTeachingC: 10
+  };
+
+  // For Cpp subject
+  console.log("C++");
+  cpp.setTeacher(cTeacher);
+  console.log(cpp.getRequirements());
+  console.log(cpp.getAvailableTeacher());
+
+  // For Java subject
+  console.log("Java");
+  java.setTeacher(cTeacher);
+  console.log(java.getRequirements());
+  console.log(java.getAvailableTeacher());
+
+  // For React subject
+  console.log("React");
+  react.setTeacher(cTeacher);
+  console.log(react.getRequirements());
+  console.log(react.getAvailableTeacher());
 }
